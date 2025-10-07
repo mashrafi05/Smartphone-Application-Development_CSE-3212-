f639b33febabc088b3c45a7b70ff120983434361
mashrafi05 <mashrfi2019new@gmail.com>
2025-10-08 02:17:13 +0600

    Add Assignment 1 Practice 2 Question 1

---
+++ Assignment 1/Practice 2/Q1.dart
@@ -0,0 +1,16 @@
+void main() {
+  int num1 = 50;
+  int num2 = 99;
+
+  if (num1 % 2 == 0) {
+    print("$num1 is even");
+  } else {
+    print("$num1 is odd");
+  }
+
+  if (num2 % 2 == 0) {
+    print("$num2 is even");
+  } else {
+    print("$num2 is odd");
+  }
+}
